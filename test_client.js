0ebe672193425208eb0459ed41baf11389e3172a
Tatsuya Minagawa <cha84rakanal@gmail.com>
2019-01-29 01:11:14 +0900

    クライアントのコードを作成

---
+++ test_client.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const fs = require('fs');
+const io = require('socket.io-client');
+
+// client-side
+const socket = io('https://localhost:8000',{
+    // option 1
+    ca: fs.readFileSync('./temp/key/server.pem'),
+  
+    // option 2. WARNING: it leaves you vulnerable to MITM attacks!
+    rejectUnauthorized: false
+});
+
+socket.on('connect', function(){});
+socket.on('event', function(data){});
+socket.on('sendMessageToClient', function(data){console.log(data)});
+socket.on('disconnect', function(){});
